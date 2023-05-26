d19a58a77e629ab8ea7b8cc85816534397126f5b
Lucas Silva <ssilvalucas2004@gmail.com>
2023-05-26 17:13:41 -0300

    some changes

--- script.js
+++ script.js
@@ -7,46 +7,66 @@ seconds.textContent = "00"
 const buttonStart = document.getElementById("buttonStart")
 buttonStart.textContent = "start"
 
+const text = document.getElementById("text")
+text.textContent = "Time to focus!"
+
+const pomodoro = () => {
+    let remaingMinutes = "25"
+    minutes.textContent = remaingMinutes
+    text.textContent = "Time to focus!"
+}
+
+const shortBrake = () => {
+    let remaingMinutes = "05"
+    minutes.textContent = remaingMinutes
+    text.textContent = "Time for a Brake!"
+}
+
+const longBrake = () => {
+    let remaingMinutes = "15"
+    minutes.textContent = remaingMinutes
+    text.textContent = "Time for a Brake!"
+}
+
 const start = () => {
-    let remaingMinutes = 0
-    let remaingSeconds = 15
+
+    let remaingMinutes = minutes.textContent
+    let remaingSeconds = 0
+
+    console.log(remaingSeconds);
 
     if(buttonStart.textContent === "start"){
+
+        buttonStart.textContent = 'restart'
+
         const countdownInterval = setInterval(() => {
             if (remaingSeconds === 0) {
-                remaingMinutes--
                 remaingSeconds = 59
-            }
-            else {
+                remaingMinutes--
+            } else {
                 remaingSeconds--
             }
             
             if (remaingSeconds >= 10){
                 seconds.textContent = remaingSeconds
-            }
-            else {
+            } else {
                 seconds.textContent = '0' + remaingSeconds
             }
             
             if (remaingMinutes >= 10){
                 minutes.textContent = remaingMinutes
-            }
-            else {
+            } else {
                 minutes.textContent = '0' + remaingMinutes
             }
 
             if (remaingMinutes === 0 && remaingSeconds === 0) {
                 clearInterval(countdownInterval)
                 buttonStart.textContent = "start"
-            }
+            }    
         }, 1000)
-        buttonStart.textContent = "restart"
     }
-
 }
 
 const restart = () => {
-    if(buttonStart.textContent === "restart"){
-        clearInterval()
-    }
+    
 }
