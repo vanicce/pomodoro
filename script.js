5ffed55ce4941f59535c6595d566d5b2cccf4998
Lucas Silva <ssilvalucas2004@gmail.com>
2023-05-29 16:35:54 -0300

    refactoring do código

--- script.js
+++ script.js
@@ -1,74 +1,79 @@
 const minutes = document.getElementById("minutes")
-minutes.textContent = "25"
-
 const seconds = document.getElementById("seconds")
-seconds.textContent = "00"
 
 const buttonStart = document.getElementById("buttonStart")
-buttonStart.textContent = "start"
+const buttonPause = document.getElementById("buttonPause")
 
 const text = document.getElementById("text")
-text.textContent = "Time to focus!"
+
+const pomodoroButton = document.getElementById("pomodoro")
+const shortBrakeButton = document.getElementById("shortBrake")
+const longBrakeButton = document.getElementById("longBrake")
 
 let countdownInterval
 
+minutes.textContent = "25"
+seconds.textContent = "00"
+text.textContent = "Time to Focus!"
+
+const setTimer = (mins, secs, timerText) => {
+    clearInterval(countdownInterval)
+    minutes.textContent = mins
+    seconds.textContent = secs
+    text.textContent = timerText
+    buttonPause.style.display = 'none'
+    pomodoroButton.classList.remove('active')
+    shortBrakeButton.classList.remove('active')
+    longBrakeButton.classList.remove('active')
+}
+
 const pomodoro = () => {
-    let remaingMinutes = "25"
-    minutes.textContent = remaingMinutes
-    seconds.textContent = remaingSeconds
-    text.textContent = "Time to focus!"
+    setTimer("25", "00", "Time to Focus!")
+    pomodoroButton.classList.add('active')
 }
 
 const shortBrake = () => {
-    let remaingMinutes = "05"
-    minutes.textContent = remaingMinutes
-    text.textContent = "Time for a Brake!"
+    setTimer("05", "00", "Time for a Brake!")
+    shortBrakeButton.classList.add('active')
 }
 
 const longBrake = () => {
-    let remaingMinutes = "15"
-    minutes.textContent = remaingMinutes
-    text.textContent = "Time for a Brake!"
+    setTimer("15", "00", "Time for a Brake!")
+    longBrakeButton.classList.add('active')
 }
 
 const start = () => {
+    const remaingMinutes = parseInt(minutes.textContent)
+    const remaingSeconds = parseInt(seconds.textContent)
 
-    let remaingMinutes = minutes.textContent
-    let remaingSeconds = seconds.textContent
+    if (remaingMinutes > 0 && remaingSeconds >= 0) {
+        buttonPause.style.display = 'block'
 
-    const buttonPause = document.getElementById("buttonPause")
-    buttonPause.textContent = "pause"
-    buttonPause.style.display = 'block'
+        countdownInterval = setInterval(() => {
+            let mins = parseInt(minutes.textContent)
+            let secs = parseInt(seconds.textContent)
 
-    countdownInterval = setInterval(() => {
-        if (remaingSeconds == 0) {
-            remaingSeconds = 59
-            remaingMinutes--
-        } else {
-            remaingSeconds--
-        }
+            if (secs === 0) {
+                secs = 59
+                mins--
+            } else {
+                secs--
+            }
 
-        if (remaingSeconds >= 10) {
-            seconds.textContent = remaingSeconds
-        } else {
-            seconds.textContent = '0' + remaingSeconds
-        }
+            seconds.textContent = secs.toString().padStart(2, '0');
+            minutes.textContent = mins.toString().padStart(2, '0');
 
-        if (remaingMinutes >= 10) {
-            minutes.textContent = remaingMinutes
-        } else {
-            minutes.textContent = '0' + remaingMinutes
-        }
-
-        if (remaingMinutes == 0 && remaingSeconds == 0) {
-            clearInterval(countdownInterval)
-            buttonStart.textContent = "start"
-            buttonPause.style.display = 'none'
-        }
-    }, 1000)
+            if (mins === 0 && secs === 0) {
+                clearInterval(countdownInterval)
+                buttonPause.style.display = 'none'
+                window.alert('O Tempo Acabou')
+            }
+        }, 1000)
+    } else {
+        window.alert('Escolha um Pomodoro ou dê uma pausa')
+    }
 }
 
 const pause = () => {
     clearInterval(countdownInterval)
-     
 }
