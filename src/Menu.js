505d84bdbd31c9c7f414545224a9fd2736a4dbf3
Lucas <ssilvalucas2004@gmail.com>
2023-09-02 23:10:20 -0300

    feat: adding menu to toggle themes

---
+++ src/Menu.js
@@ -0,0 +1,45 @@
+const Menu = {
+  render() {
+    const menu = document.createElement("div");
+
+    menu.classList.add("menu");
+
+    menu.innerHTML = `
+    <div class="menu-content">
+      <div class="menu-header">
+      <p>Themes:</p>
+        <button id="close-menu">
+          <img src="../imgs/xx.png">
+        </button>
+      </div>
+      <div class="menu-body">
+        <button id="pink-theme"> <img src="../imgs/img-pink.png"></button>
+        <button id="green-theme"> <img src="../imgs/img-project.png"></button>
+      </div>
+    </div>
+    `;
+
+    const closeMenu = menu.querySelector("#close-menu");
+
+    closeMenu.addEventListener("click", () => {
+      menu.remove();
+    });
+
+    const pinkTheme = menu.querySelector("#pink-theme");
+    const greenTheme = menu.querySelector("#green-theme");
+
+    pinkTheme.addEventListener("click", () => {
+      document.body.classList.add("pink-theme");
+      document.body.classList.remove("green-theme");
+    });
+
+    greenTheme.addEventListener("click", () => {
+      document.body.classList.add("green-theme");
+      document.body.classList.remove("pink-theme");
+    });
+
+    document.querySelector(".container").appendChild(menu);
+  },
+};
+
+export { Menu };
