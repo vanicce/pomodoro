c32db5a7bbdf4f7739dfb9cfe43f3b565323e71b
Lucas <ssilvalucas2004@gmail.com>
2023-09-06 21:42:43 -0300

    .

--- src/js/App.js
+++ src/js/App.js
@@ -2,10 +2,7 @@ import { Timer } from "./Timer.js";
 import { Buttons } from "./Buttons.js";
 import { Modal } from "./Modal.js";
 import { TextPomodoro } from "./TextPomodoro.js";
-<<<<<<< HEAD:src/js/App.js
-=======
 import { Notifications } from "./Notifications.js";
->>>>>>> 4f1841cc953d8623c0253a89be688d1d376e4669:src/App.js
 
 const App = {
   async start() {
